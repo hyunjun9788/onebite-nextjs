0b6fd56a67ace470e16f60d4c23c6756eced899e
hyunjun9788 <namhyunjun9788@gmail.com>
2025-07-06 17:03:42 +0900

    feat: SSR, ISR, SSG 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,46 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import style from "./index.module.css";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetStaticPropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+//컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+export const getStaticProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
+
+  console.log(allBooks);
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+//서버에서도 한번 실행(사전 렌더링 떄문)
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(allBooks);
+
+  useEffect(() => {
+    console.log(window);
+  });
+
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,41 @@
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
-import books from "@/mock/books.json";
+import { ReactNode, useEffect, useState } from "react";
+import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
+
+// export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+//   console.log(context);
+
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
 
 export default function Page() {
   //query string읽을 때 한번 더 호출되면서 콘솔 두번 찍힘
+
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
